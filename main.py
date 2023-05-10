725460b92cd64e913ae6045ca8127037e944cdd0
sosullivan7221 <sosullivan7221@gmail.com>
2023-05-10 23:52:34 +0000

    finished main.py file

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ def preview():
     result = top1rows.to_json(orient="records")
     return result
 
+@app.get("/payer/{value}")
+def payer(value):
+    print("value: ", value)
+    filtered = df[df["PAYER"] == value]
+    if len(filtered) <= 0:
+        return {"There is nothing here"}
+    else:
+        return filtered.to_json(orient="records")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
