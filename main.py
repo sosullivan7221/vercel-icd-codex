ab755a6f67ec0ad77a5b245d383ca1a68f1fde12
sosullivan7221 <sosullivan7221@gmail.com>
2023-05-10 23:43:42 +0000

    added preview end point

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ import uvicorn
 
 app = FastAPI()
 
+df = pd.read_csv('./data/rxsummary_small2018.csv')
+
 @app.get("/")
 def home():
     return {"Placeholder"}
+
+@app.get("/preview")
+def preview():
+    top1rows=df.head(1)
+    result = top1rows.to_json(orient="records")
+    return result
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
